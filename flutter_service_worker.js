cd587ccb3891ceb46707e4d1ec081145f96f3c17
mrmihi <mrmihi@users.noreply.github.com>
2025-05-26 12:00:20 +0000

    deploy: d37ee3ff55d3985c823cc89ee41ef1ca69f7b2cf

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "canvaskit/chromium/canvaskit.js": "8191e843020c832c9cf8852a4b909d4c",
 "canvaskit/chromium/canvaskit.wasm": "c054c2c892172308ca5a0bd1d7a7754b",
 "canvaskit/chromium/canvaskit.js.symbols": "f7c5e5502d577306fb6d530b1864ff86",
-"flutter_bootstrap.js": "d0a3ca568193605a009fef202c3347c8",
+"flutter_bootstrap.js": "c94bf63ceefc6095ead8e7cdd21648b3",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "51fcc9ee73febcdb718b879ab8f7da33",
 "/": "51fcc9ee73febcdb718b879ab8f7da33",
