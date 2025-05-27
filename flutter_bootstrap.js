f40bf9903e5dff95b9b3731e6eac939f999160c3
mrmihi <mrmihi@users.noreply.github.com>
2025-05-27 10:10:44 +0000

    deploy: 551ffbf3669f12ff6889838c0fdd918d404893af

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2359057459"
+    serviceWorkerVersion: "3137722382"
   }
 });
